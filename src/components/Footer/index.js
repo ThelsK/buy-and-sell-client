71e3948b6e3ff538bc3dc8c3fba93256c5504208
ThelsK <github@thels.nl>
2019-09-11 15:05:00 +0200

    Improved footer buttons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,16 +5,12 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="Footer">
-        <div>
-          <Link to="/newad">
-            Add your own advertisement
+        <Link className="FooterLink" to="/newad">
+          Add your own advertisement
           </Link >
-        </div>
-        <div>
-          <Link to="/">
-            Return to the overview
+        <Link className="FooterLink" to="/">
+          Return to the overview
           </Link >
-        </div>
       </div >
     )
   }
